fd18a6c48a7803743469dcfcfdc0e17f1d0043e3
UnHold (Alex) <alex@hold.re>
2024-12-01 11:08:33 +0100

    added day1

---
+++ 2024/day01/day01.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Text (splitOn, pack, unpack)
+import Data.List(sort)
+
+task1 :: IO ()
+task1 = do
+    input <- readFile "input1.txt"
+    let nums :: [[Integer]] = map (map (read . unpack) . splitOn "   " . pack) (lines input)
+    print . sum $ zipWith (\a b -> abs (a - b)) (sort (map head nums)) (sort (map last nums))
+
+
+task2 :: IO ()
+task2 = do
+    input <- readFile "input1.txt"
+    let nums :: [[Integer]] = map (map (read . unpack) . splitOn "   " . pack) (lines input)
+    print $ sum [sum [y | y <-  map last nums, y == x] | x <- map head nums]
